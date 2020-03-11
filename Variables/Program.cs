f8e4e58aebb7225c1af3b77bb84d694e56eb5574
Nepcam <cameron.nepe@gmail.com>
2020-03-11 21:53:55 +1300

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Console.WriteLine("Number x is " + x);
+            x = 5;
+            Console.WriteLine("Number x is " + x);
+
+            double double_num = 0.0d;
+            float float_num = 0.0f;
+            decimal decimal_num = 0.0m;
+
+            double_num = 1.5d;
+            float_num = 1.5f;
+            decimal_num = 2.0m;
+
+            Console.WriteLine("Double is " + double_num + " Float is " + float_num + " Decimal is " + decimal_num);
+
+            string firstName, lastName, House;
+            firstName = "Jon";
+            lastName = " Snow";
+            House = " House Stark";
+
+            Console.WriteLine("Hello, " + firstName + lastName + " from -" + House);
+        }
+    }
+}
